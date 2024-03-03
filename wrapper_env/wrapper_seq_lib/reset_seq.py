4e1190d1bc8a3686c9f4bf53b01a118f21f40f12
M0stafaRady <mostafa.rady@efabless.com>
2024-03-03 18:06:42 +0200

    fix the reset sequence import

--- wrapper_env/wrapper_seq_lib/reset_seq.py
+++ wrapper_env/wrapper_seq_lib/reset_seq.py
@@ -3,10 +3,10 @@ from uvm.macros.uvm_object_defines import uvm_object_utils
 from uvm.macros.uvm_message_defines import uvm_info, uvm_fatal
 from uvm.macros.uvm_sequence_defines import uvm_do_with, uvm_do
 from uvm.base import UVM_LOW
-from uart_seq_lib.seq_base import seq_base
+from EF_UVM.wrapper_env.wrapper_seq_lib.wrapper_seq_base import wrapper_seq_base
 
 
-class reset_seq(seq_base):
+class reset_seq(wrapper_seq_base):
     def __init__(self, name="reset_seq"):
         super().__init__(name)
 
