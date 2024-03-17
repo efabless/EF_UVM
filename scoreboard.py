add6d5f56cf0cfa8f5337fea69b84412de0e8445
NouranAbdelaziz <nouran.abdelaziz@aucegpt.edu>
2024-03-17 17:18:32 +0200

    updated scoreboard to check on queue if it has one item

--- scoreboard.py
+++ scoreboard.py
@@ -106,11 +106,11 @@ class scoreboard(UVMScoreboard):
     def extract_phase(self, phase):
         super().extract_phase(phase)
         # check all the quese is empty or at least has the same item
-        if self.q_bus.qsize() not in [0, 1] or  self.q_bus_ref_model.qsize() not in [0, 1]:
+        if self.q_bus.qsize() != 0 or  self.q_bus_ref_model.qsize() not in [0, 1]:
             uvm_error(self.tag, f"Bus queue still have unchecked items queue bus {self.q_bus._queue} size {self.q_bus.qsize()} bus_ref_model {self.q_bus_ref_model._queue} size {self.q_bus_ref_model.qsize()}")
-        if self.q_irq.qsize() not in [0, 1] or self.q_irq_ref_model.qsize() not in [0, 1]:
+        if self.q_irq.qsize() != 0 or self.q_irq_ref_model.qsize() not in [0, 1]:
             uvm_error(self.tag, f"IRQ queue still have unchecked items queue irq {self.q_irq._queue} size {self.q_irq.qsize()} irq_ref_model {self.q_irq_ref_model._queue} size {self.q_irq_ref_model.qsize()}")
-        if self.q_ip.qsize() not in [0, 1] or self.q_ip_ref_model.qsize() not in [0, 1]:
+        if self.q_ip.qsize() != 0 or self.q_ip_ref_model.qsize() not in [0, 1]:
             uvm_error(self.tag, f"IP queue still have unchecked items queue ip {self.q_ip._queue} size {self.q_ip.qsize()} ip_ref_model {self.q_ip_ref_model._queue} size {self.q_ip_ref_model.qsize()}")
 
 
