aa90f92a57249527ff31775cc2ed112a8e6df492
Kareem Farid <kareefardi@users.noreply.github.com>
2024-04-16 18:23:31 +0200

    ??

---
+++ .github/scripts/get_tests_matrix_alt_2.py
@@ -0,0 +1,14 @@
+import json
+import sys
+
+
+def main():
+    tests = sys.argv[1:]
+    output_matrix = {"tests": []}
+    for test in tests:
+        output_matrix["tests"].append({"test": test})
+    print(json.dumps(output_matrix))
+
+
+if __name__ == "__main__":
+    main()
