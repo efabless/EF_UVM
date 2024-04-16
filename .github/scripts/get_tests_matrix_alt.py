19f3a27006e4748d9fe45b6fa10e2822b33f9c70
Kareem Farid <kareefardi@users.noreply.github.com>
2024-04-16 17:26:24 +0200

    Better matrix attempt

---
+++ .github/scripts/get_tests_matrix_alt.py
@@ -0,0 +1,25 @@
+import yaml
+import json
+
+
+def main():
+    test_set_yaml = ".github/test_set.yaml"
+    output_matrix = {}
+    test_set_stream = open(test_set_yaml)
+    data = yaml.load(test_set_stream, Loader=yaml.Loader)
+    for item in data:
+        tests = item["tests"]
+        name = item["name"]
+        if output_matrix.get(name) is None:
+            output_matrix[name] = {"test-names": []}
+        url = item["url"]
+        output_matrix[name]["url"] = url
+        for test in tests:
+            output_matrix[name]["test-names"].append(test)
+
+    print(json.dumps(output_matrix, indent=4))
+    test_set_stream.close()
+
+
+if __name__ == "__main__":
+    main()
