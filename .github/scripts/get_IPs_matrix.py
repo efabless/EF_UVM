52f4edc9abe0e6c9405c0cd5232fc9d07c1cb3a2
Kareem Farid <kareefardi@users.noreply.github.com>
2024-04-17 11:19:18 +0200

    More typos and renames

--- .github/scripts/get_IPs_matrix.py
+++ .github/scripts/get_IPs_matrix.py
@@ -1,14 +1,22 @@
+import yaml
 import json
-import sys
 
 
 def main():
-    tests = sys.argv[1:]
-    tests = " ".join(tests).split(",")
+    test_set_yaml = ".github/test_set.yaml"
     output_matrix = {"IPs": []}
-    for test in tests:
-        output_matrix["IPs"].append({"test": test})
+    test_set_stream = open(test_set_yaml)
+    data = yaml.load(test_set_stream, Loader=yaml.Loader)
+    for item in data:
+        tests = item["tests"]
+        name = item["name"]
+        url = item["url"]
+        output_matrix["IPs"].append(
+            {"name": name, "url": url, "test-names": ",".join(tests)}
+        )
+
     print(json.dumps(output_matrix))
+    test_set_stream.close()
 
 
 if __name__ == "__main__":
--- .github/scripts/get_tests_matrix.py
+++ .github/scripts/get_tests_matrix.py
@@ -1,22 +1,14 @@
-import yaml
 import json
+import sys
 
 
 def main():
-    test_set_yaml = ".github/test_set.yaml"
+    tests = sys.argv[1:]
+    tests = " ".join(tests).split(",")
     output_matrix = {"tests": []}
-    test_set_stream = open(test_set_yaml)
-    data = yaml.load(test_set_stream, Loader=yaml.Loader)
-    for item in data:
-        tests = item["tests"]
-        name = item["name"]
-        url = item["url"]
-        output_matrix["tests"].append(
-            {"name": name, "url": url, "test-names": ",".join(tests)}
-        )
-
+    for test in tests:
+        output_matrix["tests"].append({"test": test})
     print(json.dumps(output_matrix))
-    test_set_stream.close()
 
 
 if __name__ == "__main__":
