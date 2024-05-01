8111c02302ef6dee86f3bcce3ff0fad8ea7341f6
M0stafaRady <mostafa.rady@efabless.com>
2024-05-01 17:21:24 +0300

    continue pipe debugging

--- bus_env/bus_item.py
+++ bus_env/bus_item.py
@@ -17,7 +17,7 @@ class bus_item(UVMSequenceItem):
     WRITE = 1
     RESET = 2
     NOPE = 3  # Insert a no-op in the sequence
-
+    counter = 0
     def __init__(self, name="bus_item"):
         super().__init__(name)
         self.tag = name
@@ -27,6 +27,8 @@ class bus_item(UVMSequenceItem):
         self.rand("data", range(0, 0xFFFF))
         self.kind = bus_item.READ  # kind_e
         self.rand("kind", [bus_item.READ, bus_item.WRITE])
+        self.id = bus_item.counter
+        bus_item.counter += 1
 
     def convert2string(self):
         if self.kind == bus_item.RESET:
